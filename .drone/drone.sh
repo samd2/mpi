1568dbfd1eb80699b311ad539e3b0d8295f7b56e
sdarwin <samuel.d.darwin@gmail.com>
2021-01-28 16:09:34 +0000

    test

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-set -ex
+# Copyright 2020 Rene Rivera, Sam Darwin
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+set -e
 export TRAVIS_BUILD_DIR=$(pwd)
+export DRONE_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=$DRONE_BRANCH
-export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
 export VCS_COMMIT_ID=$DRONE_COMMIT
 export GIT_COMMIT=$DRONE_COMMIT
-export DRONE_CURRENT_BUILD_DIR=$(pwd)
+export REPO_NAME=$DRONE_REPO
 export PATH=~/.local/bin:/usr/local/bin:$PATH
 
-echo '==================================> BEFORE_INSTALL'
-
-. .drone/before-install.sh
+if [ "$DRONE_JOB_BUILDTYPE" == "boost" ]; then
 
 echo '==================================> INSTALL'
 
@@ -27,17 +29,11 @@ python tools/boostdep/depinst/depinst.py mpi
 ./bootstrap.sh
 ./b2 headers
 
-echo '==================================> BEFORE_SCRIPT'
-
-. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
-
 echo '==================================> SCRIPT'
 
 echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
-echo "using mpi : mpic++.openmpi : : mpirun.openmpi ;" >> ~/user-config.jam
+echo "using mpi : mpic++.openmpi : : mpirun.openmpi --allow-run-as-root ;" >> ~/user-config.jam
 mpirun.openmpi --version
 ./b2 -j3 libs/mpi/test toolset=$TOOLSET cxxstd=$CXXSTD variant=release
 
-echo '==================================> AFTER_SUCCESS'
-
-. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
+fi
